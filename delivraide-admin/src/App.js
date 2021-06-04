3267b13300455309733caae5162473e6ed877d8d
Robert Mathéo <matheo.robert@epitech.eu>
2021-06-04 22:26:04 +0200

    Initial commit => create-react-app cleaned + setup tailwind

---
+++ delivraide-admin/src/App.js
@@ -0,0 +1,11 @@
+import './styles/index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="flex-auto text-xl font-bold">Home</h1>
+    </div>
+  );
+}
+
+export default App;
