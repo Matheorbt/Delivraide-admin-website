0219c4afa028adb6c5e986d211bc9f595530cc5c
Mathéo Robert <57138558+Matheorbt@users.noreply.github.com>
2021-06-10 11:10:38 +0200

    Working on navbar

--- delivraide-admin/src/components/Navigation/NavigationBar.js
+++ delivraide-admin/src/components/Navigation/NavigationBar.js
@@ -1,62 +1,53 @@
 import React from 'react'
 import {
-    Link
+    Link,
+    useLocation
 } from "react-router-dom";
 
 import logoDelivraide from '../../assets/logo/logo.png'
+import profilePictureDelivraide from '../../assets/logo/delivraide-icon-192.png'
 
 // React Bootstrap import
-import { Button, Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { Image, Navbar, Container, Nav, NavDropdown, Row, Col } from 'react-bootstrap';
 
+function HeaderView() {
+    const location = useLocation();
+    if (location.pathname === "/")
+        return <span>Accueil</span>
+    else {
+        let currentPage = location.pathname.split('/')[2];
+        currentPage = currentPage.charAt(0).toUpperCase() + currentPage.slice(1)
+        return <span>{currentPage}</span>
+    }
+}
 function NavigationBar() {
     return (
         <>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="/"><img src={logoDelivraide} width="250" alt="logo Delivr'aide"></img></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/">Accueil</Nav.Link>
-                        <NavDropdown title="Livreurs" id="basic-nav-dropdown">
-                            <NavDropdown.Item as={Link} to="/livreur/candidature">Candidatures</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/livreur/ajouter">Ajouter</NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title="News" id="basic-nav-dropdown">
-                            <NavDropdown.Item as={Link} to="/news/liste">Liste</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/news/ajouter">Ajouter</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <Link to="/">Accueil</Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link to="/livreur">Livreur</Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link to="/news">News</Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className="navbar-top-section">
-                    <div className="left-section">
-                        <div className="dropdown-menu">
-                            <h1>Drop down menu</h1>
-                        </div>
-                        <div className="current-page-name">
-                            <h1>Current page</h1>
-                        </div>
-                    </div>
-                    <div className="middle-section">
-                        <div></div>
-                    </div>
-                    <div className="right-section">
-                        <h1 className="profile-picture-preview">pp</h1>
-                        <h1 className="account-name">Didier Deschamps</h1>
-                    </div>
-                </div>
+            <Navbar bg="light" expand="never">
+                <Container>
+                    <Container fluid="xl">
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <h1>{HeaderView()}</h1>
+                        <Navbar.Brand href="/"><img src={logoDelivraide} width="250" alt="logo Delivr'aide"></img></Navbar.Brand>
+                    </Container>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Row>
+                                <Col xs={5}><Image src={profilePictureDelivraide} width="100" /></Col>
+                                <Col><h1 className="account-name">Didier Deschamps</h1></Col>
+                            </Row>
+                            <Nav.Link as={Link} to="/">Accueil</Nav.Link>
+                            <NavDropdown title="Livreurs" id="basic-nav-dropdown">
+                                <NavDropdown.Item as={Link} to="/livreur/candidature">Candidatures</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/livreur/ajouter">Ajouter</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="News" id="basic-nav-dropdown">
+                                <NavDropdown.Item as={Link} to="/news/liste">Liste</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/news/ajouter">Ajouter</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
         </>
     )
--- delivraide-admin/src/components/News/News.js
+++ delivraide-admin/src/components/News/News.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import {
     Switch,
-    Route,
-    Link,
+    Route
 } from "react-router-dom";
 import NavigationBar from '../Navigation/NavigationBar'
 
